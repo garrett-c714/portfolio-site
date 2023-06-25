710ff7ba348dda7a4496c4b20d89f62260329871
Garrett Cox <gcox5714@gmail.com>
2023-06-25 17:53:46 -0400

    Started working on Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {useState} from 'react'
+
+const Navbar = () => {
+
+    return (
+	<>
+	    <div className="nav-container">
+		<div className="nav-links">
+		    <p className="nav-underline">About Me</p>
+		    <p>Projects</p>
+		    <p>Work Experience</p>
+		</div>
+	    </div>
+	</>
+    );
+    
+}
+
+export default Navbar
